4e34eb7e260eba349ff66d1edf471014b5d67e86
John M. Wargo <johnwargo@users.noreply.github.com>
2023-06-19 18:02:33 -0400

    swapped template

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Vortex by Pixelarity
+	Dopetrope by Pixelarity
 	pixelarity.com | hello@pixelarity.com
 	License: pixelarity.com/license
 */
@@ -7,17 +7,14 @@
 (function($) {
 
 	var	$window = $(window),
-		$header = $('#header'),
 		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -27,48 +24,31 @@
 			}, 100);
 		});
 
-	// Tweaks/fixes.
-
-		// Polyfill: Object fit.
-			if (!browser.canUse('object-fit')) {
-
-				$('.image[data-position]').each(function() {
-
-					var $this = $(this),
-						$img = $this.children('img');
-
-					// Apply img as background.
-						$this
-							.css('background-image', 'url("' + $img.attr('src') + '")')
-							.css('background-position', $this.data('position'))
-							.css('background-size', 'cover')
-							.css('background-repeat', 'no-repeat');
-
-					// Hide img.
-						$img
-							.css('opacity', '0');
-
-				});
-
-			}
-
 	// Dropdowns.
 		$('#nav > ul').dropotron({
-			alignment: 'right',
-			hideDelay: 350,
-			baseZIndex: 100000
+			mode: 'fade',
+			noOpenerFade: true,
+			alignment: 'center'
 		});
 
-	// Menu.
-		$('<a href="#navPanel" class="navPanelToggle">Menu</a>')
-			.appendTo($header);
+	// Nav.
 
-		$(	'<div id="navPanel">' +
-				'<nav>' +
-					$('#nav') .navList() +
-				'</nav>' +
-				'<a href="#navPanel" class="close"></a>' +
-			'</div>')
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
 				.appendTo($body)
 				.panel({
 					delay: 500,
@@ -76,7 +56,9 @@
 					hideOnSwipe: true,
 					resetScroll: true,
 					resetForms: true,
-					side: 'right'
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
 				});
 
 })(jQuery);
